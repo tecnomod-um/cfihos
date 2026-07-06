bf4e212a2ae8f444fa8dc42838af147934c501f7
jesualdotomasfernandezbreis <jfernand@um.es>
2026-07-06 20:37:01 +0200

    Update test-consistency-cfihos-ido.py

--- test/test-consistency-cfihos-ido.py
+++ test/test-consistency-cfihos-ido.py
@@ -1,5 +1,5 @@
  python3 validate_ontology_consistency.py \
-        --ontology CFIHOS=../ontology/CORE-CFIHOS-V2.0_ido.owl \
+        --ontology CFIHOSIDO=../ontology/CORE-CFIHOS-V2.0_ido.owl \
         --reasoner hermit \
         --with-imports \
         --onto-path ../ontology/ \
