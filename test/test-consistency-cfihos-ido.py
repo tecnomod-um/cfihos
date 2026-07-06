de77cb7e80a2171122a0bbcaeb359878aa39f77e
jesualdotomasfernandezbreis <jfernand@um.es>
2026-07-06 21:00:08 +0200

    remove consistency script

--- test/test-consistency-cfihos-ido.py
+++
@@ -1,6 +0,0 @@
- python3 validate_ontology_consistency.py \
-        --ontology CFIHOSIDO=../ontology/CORE-CFIHOS-V2.0_ido.owl \
-        --reasoner hermit \
-        --with-imports \
-        --onto-path ../ontology/ \
-        --out-dir ./consistency_reports 
--- test/test-consistency-cfihos.py
+++
@@ -1,4 +0,0 @@
- python3 validate_ontology_consistency.py \
-        --ontology CFIHOS=../ontology/CORE-CFIHOS-V2.0.owl \
-        --reasoner hermit \
-        --out-dir ./consistency_reports 
