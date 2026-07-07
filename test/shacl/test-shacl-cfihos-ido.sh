478faae6485ff59c8da0db5de2fb767e730f7482
jesualdotomasfernandezbreis <jfernand@um.es>
2026-07-07 12:23:04 +0200

    update files

--- test/shacl/test-shacl-cfihos-ido.sh
+++ test/shacl/test-shacl-cfihos-ido.sh
@@ -1,4 +1,4 @@
-pyshacl -d data-examples/example-B.ttl  -s CFIHOS-IDO/cfihos-ido-shapes-astrea.ttl -e CFIHOS-IDO/cfihos-ido-shapes-astrea.ttl --inference rdfs -o CFIHOS-IDO/report-example-B-astrea.txt
+pyshacl -d data-examples/example-CFIHOS-IDO.ttl  -s CFIHOS-IDO/cfihos-ido-shapes-astrea.ttl -e CFIHOS-IDO/cfihos-ido-shapes-astrea.ttl --inference rdfs -o CFIHOS-IDO/report-example-CFIHOS-IDO-astrea.txt
 pyshacl -d data-examples/valve-instances-compliant.ttl  -s CFIHOS-IDO/cfihos-ido-shapes-astrea.ttl -e CFIHOS-IDO/cfihos-ido-shapes-astrea.ttl --inference rdfs -o CFIHOS-IDO/report-valve-instances-compliant-astrea.txt
 pyshacl -d data-examples/valve-instances-noncompliant.ttl  -s CFIHOS-IDO/cfihos-ido-shapes-astrea.ttl -e CFIHOS-IDO/cfihos-ido-shapes-astrea.ttl --inference rdfs -o CFIHOS-IDO/report-valve-instances-noncompliant-astrea.txt
 pyshacl -d data-examples/valve-compliant-with-cfihos-only.ttl  -s CFIHOS-IDO/cfihos-ido-shapes-astrea.ttl -e CFIHOS-IDO/cfihos-ido-shapes-astrea.ttl --inference rdfs -o CFIHOS-IDO/report-valve-compliant-with-cfihos-only-astrea.txt
@@ -6,7 +6,7 @@ pyshacl -d data-examples/valve-compliant-with-ido-only.ttl  -s CFIHOS-IDO/cfihos
 pyshacl -d data-examples/cfihos_ido_valid_data_shapes_test.ttl  -s CFIHOS-IDO/cfihos-ido-shapes-astrea.ttl -e CFIHOS-IDO/cfihos-ido-shapes-astrea.ttl --inference rdfs -o CFIHOS-IDO/report-cfihos_ido-valid_data_shapes_test-astrea.txt
 pyshacl -d data-examples/cfihos_ido_invalid_data_shapes_test.ttl  -s CFIHOS-IDO/cfihos-ido-shapes-astrea.ttl -e CFIHOS-IDO/cfihos-ido-shapes-astrea.ttl --inference rdfs -o CFIHOS-IDO/report-cfihos_ido-invalid_data_shapes_test-astrea.txt
 
-pyshacl -d data-examples/example-B.ttl  -s CFIHOS-IDO/cfihos-ido-shapes-data.ttl -e CFIHOS-IDO/cfihos-ido-shapes-data.ttl --inference rdfs -o CFIHOS-IDO/report-example-B-data.txt
+pyshacl -d data-examples/example-CFIHOS-IDO.ttl  -s CFIHOS-IDO/cfihos-ido-shapes-data.ttl -e CFIHOS-IDO/cfihos-ido-shapes-data.ttl --inference rdfs -o CFIHOS-IDO/report-example-CFIHOS-IDO-data.txt
 pyshacl -d data-examples/valve-instances-compliant.ttl  -s CFIHOS-IDO/cfihos-ido-shapes-data.ttl -e CFIHOS-IDO/cfihos-ido-shapes-data.ttl --inference rdfs -o CFIHOS-IDO/report-valve-instances-compliant-data.txt
 pyshacl -d data-examples/valve-instances-noncompliant.ttl  -s CFIHOS-IDO/cfihos-ido-shapes-data.ttl -e CFIHOS-IDO/cfihos-ido-shapes-data.ttl --inference rdfs -o CFIHOS-IDO/report-valve-instances-noncompliant-data.txt
 pyshacl -d data-examples/valve-compliant-with-cfihos-only.ttl  -s CFIHOS-IDO/cfihos-ido-shapes-data.ttl -e CFIHOS-IDO/cfihos-ido-shapes-data.ttl --inference rdfs -o CFIHOS-IDO/report-valve-compliant-with-cfihos-only-data.txt
